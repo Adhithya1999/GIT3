f007774a7e83285eeef4475dd18c3e4b63ec5bd1
Adhithya1999 <anirudhadhithya20@gmail.com>
2022-06-21 15:57:31 +0530

    changed something in sachin output

--- Sachin.java
+++ Sachin.java
@@ -2,7 +2,7 @@ public class Sachin {
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
-    System.out.println("TENDULKAR");
+    System.out.println("PRIDE");
 
  }
 
